411a8ab57fe8ea8050be98ca56fee757300dbd61
Joel Cross <joel@kazbak.co.uk>
2013-11-09 18:19:32 +0000

    Pickle last tweeted date so as to not repeatedly tweet the same stuff

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
 from flask import Flask, request, abort
 
-from biblebot.tweeter import DebugTweeter, Tweeter
+from biblebot.tweeter import DebugTweeter, Tweeter, PickleFileDoesNotExist
 
 app = Flask(__name__)
 
-def debug_tweet_all():
+def debug_tweet_all(force_today=False):
     html = ['<h1 style="color: red">ATTENTION: DEBUG MODE IS ON</h1>',
             '<p>(This means that no tweets have actually been sent.)</p>',
             '<p>The following tweets <em>would</em> have been sent:</p>']
-    for tweet in DebugTweeter().tweet_all():
+    for tweet in DebugTweeter().tweet_all(force_today=force_today):
         html.append('<blockquote>{}</blockquote>'.format(tweet))
     return os.linesep.join(html)
 
 
-def tweet_all():
+def tweet_all(force_today=False):
     kwargs = {}
     for key in ['consumer_key', 'consumer_secret', 'access_token_key',
                 'access_token_secret']:
@@ -22,7 +22,7 @@ def tweet_all():
         kwargs[key] = os.getenv(env_key)
         if kwargs[key] is None:
             return 'Please set "{}" in Heroku'.format(env_key)
-    num = Tweeter(**kwargs).tweet_all()
+    num = Tweeter(**kwargs).tweet_all(force_today=force_today)
     return 'Sent {} tweet(s)'.format(num)
 
 
@@ -32,7 +32,13 @@ def cron():
     if api_key is None:
         return 'Please set an API key in Heroku'
     if request.args.get('key') and request.args.get('key') == api_key:
+        force_today = 'force_today' in request.args
         if app.debug:
-            return debug_tweet_all()
-        return tweet_all()
+            func = debug_tweet_all
+        else:
+            func = tweet_all
+        try:
+            return func(force_today=force_today)
+        except PickleFileDoesNotExist:
+            return 'Unable to calculate date of last tweet'
     abort(401)
--- biblebot/tweeter.py
+++ biblebot/tweeter.py
@@ -1,9 +1,15 @@
-from datetime import date#, timedelta
+import cPickle
+from datetime import date, timedelta
+import os
 
 import twitter
 
-from biblebot.constants import TEMPLATE
+from biblebot.constants import TEMPLATE, LAST_TWEETED_FILENAME
 from biblebot.data import readings
+
+
+class PickleFileDoesNotExist(Exception):
+    pass
 
 
 class Tweeter(object):
@@ -15,11 +21,27 @@ class Tweeter(object):
                                access_token_key=access_token_key,
                                access_token_secret=access_token_secret)
 
+    def _get_last_tweeted(self):
+        if not hasattr(self, '_last_tweeted_date'):
+            if not os.path.exists(LAST_TWEETED_FILENAME):
+                raise PickleFileDoesNotExist(LAST_TWEETED_FILENAME)
+            with open(LAST_TWEETED_FILENAME) as fh:
+                self._last_tweeted_date = cPickle.load(fh)
+        return self._last_tweeted_date
+
+    def _set_last_tweeted(self, value):
+        self._last_tweeted_date = value
+        with open(LAST_TWEETED_FILENAME, 'w') as fh:
+            cPickle.dump(value, fh)
+
+    last_tweeted_date = property(_get_last_tweeted, _set_last_tweeted)
+
     def get_relevant_dates(self, until):
-        # TODO proper date range
-        # see http://stackoverflow.com/questions/1060279/
-        #     iterating-through-a-range-of-dates-in-python
-        return [until]
+        one_day = timedelta(days=1)
+        start_date = self.last_tweeted_date + one_day
+        end_date = until + one_day
+        for n in xrange(int((end_date - start_date).days)):
+            yield last_tweeted_date + timedelta(n)
 
     def send_tweets(self, dt):
         tweets = readings.get(dt.month, {}).get(dt.day)
@@ -27,11 +49,18 @@ class Tweeter(object):
             self.api.PostUpdate(TEMPLATE.format(tweet))
         return len(tweets)
 
-    def tweet_all(self):
+    def tweet_all(self, force_today=False):
         today = date.today()
         tweeted = 0
-        for dt in self.get_relevant_dates(today):
+        if force_today:
+            # We use force_today to forcibly set last_tweeted_date
+            dates = [today]
+        else:
+            dates = self.get_relevant_dates(today)
+        for dt in dates:
             tweeted += self.send_tweets(dt)
+        # We only do this once to reduce IO overhead
+        self.last_tweeted_date = today
         return tweeted
 
 
@@ -46,6 +75,6 @@ class DebugTweeter(Tweeter):
     def __init__(self):
         self.api = _DebugApi()
 
-    def tweet_all(self):
-        super(DebugTweeter, self).tweet_all()
+    def tweet_all(self, *args, **kwargs):
+        super(DebugTweeter, self).tweet_all(*args, **kwargs)
         return list(self.api)
