2a102bb2e4b465969387ba743701376d90846a7e
Joel Cross <joel@kazbak.co.uk>
2013-12-26 21:21:22 +0000

    Add readings for January

--- biblebot/data.py
+++ biblebot/data.py
@@ -1,6 +1,128 @@
 from biblebot.constants import *
 
+__all__ = ['old_testament', 'new_testament', 'psalms', 'proverbs', 'readings']
+
+old_testament = {}
+new_testament = {}
+psalms = {}
+proverbs = {}
+
+old_testament[JANUARY] = {
+    1: 'Genesis 1-2:17',
+    2: 'Gen 2:18-4:16',
+    3: 'Gen 4:17-6:22',
+    4: 'Gen 7-9:17',
+    5: 'Gen 9:18-11:9',
+    6: 'Gen 11:10-13',
+    7: 'Gen 14-16',
+    8: 'Gen 17-18',
+    9: 'Gen 19:-20',
+    10: 'Gen 21-23',
+    11: 'Gen 24',
+    12: 'Gen 25-26',
+    13: 'Gen 27-28',
+    14: 'Gen 29-30',
+    15: 'Gen 31',
+    16: 'Gen 32-33',
+    17: 'Gen 34-35',
+    18: 'Gen 36-37',
+    19: 'Gen 38-39',
+    20: 'Gen 40-41:40',
+    21: 'Gen 41:41-42',
+    22: 'Gen 43-44',
+    23: 'Gen 45-47:12',
+    24: 'Gen 47-48',
+    25: 'Gen 49-50',
+    26: 'Job 1-3',
+    27: 'Job 4-7',
+    28: 'Job 8-10',
+    29: 'Job 11-14',
+    30: 'Job 15-18',
+    31: 'Job 19-21',
+}
+
+new_testament[JANUARY] = {
+    1: 'Matthew 1',
+    2: 'Mt 2:1-18',
+    3: 'Mt 2:18-3:17',
+    4: 'Mt 4',
+    5: 'Mt 4:23-5:20',
+    6: 'Mt 5:21-42',
+    7: 'Mt 5:43-6:24',
+    8: 'Mt 6:25-7:23',
+    9: 'Mt 7:24-8:22',
+    10: 'Mt 8:23-9:13',
+    11: 'Mt 9:14-38',
+    12: 'Mt 10:1-31',
+    13: '> Mt 11:15',
+    14: '> Mt 11:30',
+    15: '> Mt 12:21',
+    16: '> Mt 12:45',
+    17: '> Mt 13:17',
+    18: '> Mt 13:35',
+    19: '> Mt 13:58',
+    20: '> Mt 14:21',
+    21: '> Mt 15:9',
+    22: '> Mt 15:10-39',
+    23: '> Mt 16:20',
+    24: '> Mt 17:13',
+    25: '> Mt 18:9',
+    26: '> Mt 18:35',
+    27: '> Mt 19:15',
+    28: '> Mt 19:30',
+    29: '> Mt 20:19',
+    30: '> Mt 20:34',
+    31: '> Mt 21:27',
+}
+
+
+psalms[JANUARY] = {
+    1: 'Psalm 1',
+    2: 'Ps 2',
+    3: 'Ps 3',
+    5: 'Ps 4',
+    6: 'Ps 5',
+    7: 'Ps 6',
+    9: 'Ps 7',
+    11: 'Ps 8',
+    13: 'Ps 9',
+    17: 'Ps 10',
+    19: 'Ps 11',
+    21: 'Ps 12',
+    22: 'Ps 13',
+    23: 'Ps 14',
+    25: 'Ps 15',
+    26: 'Ps 16',
+    27: 'Ps 17',
+    31: 'Ps 18',
+}
+
+
+proverbs[JANUARY] = {
+    4: 'Prov1:1-7',
+    8: 'Prov 1:8-19',
+    12: 'Pr 1:20-33',
+    16: 'Pr 2:1-11',
+    20: 'Pr 2:12-22',
+    24: 'Pr 3:1-10',
+    28: 'Pr 3:11-20',
+}
+
+
+def _format_readings(month):
+    data = {}
+    for day in xrange(1, len(old_testament[month])):
+        reading = [old_testament[month][day], new_testament[month][day]]
+        if day in psalms[month]:
+            reading.append(psalms[month][day])
+        if day in proverbs[month]:
+            reading.append(proverbs[month][day])
+        data[day] = tuple(reading)
+    return data
+
+
 readings = {
+    JANUARY: _format_readings(JANUARY),
     NOVEMBER: {
         1: ('Joel 1:1-2:17', 'Hebrews 3:1-19'),
         2: ('>Joel 3:21', '>Heb 4:13'),
